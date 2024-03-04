cd9c9544f1fa6ce7541a98e78fc04d7e2ad96908
Arguelless <marguelles@protonmail.com>
2024-03-04 17:51:27 +0100

    dsaf2

--- src/main/java/cat/iesesteveterradas/dbapi/persistencia/UsuariDAO.java
+++ src/main/java/cat/iesesteveterradas/dbapi/persistencia/UsuariDAO.java
@@ -212,7 +212,7 @@ public class UsuariDAO {
         Session session = SessionFactoryManager.getSessionFactory().openSession();
         Usuari usuari = null;
         try {
-            Query<Usuari> query = session.createQuery("FROM Usuari WHERE api_key = :apikey", Usuari.class);
+            Query<Usuari> query = session.createQuery("FROM Usuari WHERE api_key = :'apikey'", Usuari.class);
             query.setParameter("apikey", apiKey);
             usuari = query.uniqueResult();
         } catch (HibernateException e) {
